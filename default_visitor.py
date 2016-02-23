867d66cc1e1a7990294c5c6fbe6670aa24d34b93
Zhao Yonggang <yonggang.zhao@icloud.com>
2016-02-23 17:34:32 +0800

    modified

--- default_visitor.py
+++ default_visitor.py
@@ -3,32 +3,33 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-
+def element_to_string(element):
+	return ET.tostring(element).replace("\r", "").replace("\n", "").replace("\t", "")
 
 class StdVisitor:
 	def out(self, path, op, content):
 		print("\t\t\t".join(["".join([e.tag for e in path ]), op, content]))
 	
 	def attribAdd(self, path, other_element, attribAdd):
-		self.out(path, "modify", ET.tostring(other_element)+" attribute "+str(attribAdd)+" are added")
+		self.out(path, "modify", element_to_string(other_element)+" attribute "+str(attribAdd)+" are added")
 	
 	def attribRemove(self, path, element, attribs):
-		self.out(path, "modify", ET.tostring(element)+" attribute "+str(attribs)+" are removed")
+		self.out(path, "modify", element_to_string(element)+" attribute "+str(attribs)+" are removed")
 
 	def attribModify(self, path, self_element, other_element, attribs):
-		self.out(path, "modify", "from "+ET.tostring(self_element)+" to "+ET.tostring(other_element) + " attribs "+str(attribs)+" are modified")
+		self.out(path, "modify", "from "+element_to_string(self_element)+" to "+ET.tostring(other_element) + " attribs "+str(attribs)+" are modified")
 
 	def childElementAdd(self, path, element, children):
 		for child in children:
 			newpath = path[0:]
 			newpath.append(child)
-			self.out( newpath, "add", "node "+ET.tostring(child).replace("\n", ""))
+			self.out( newpath, "add", "node "+element_to_string(child))
 
 	def childElementRemove(self, path, self_element, children):
 		for child in children:
 			newpath = path[0:]
 			newpath.append(child)
-			self.out( newpath, "remove", "node "+ET.tostring(child).replace("\n", ""))
+			self.out( newpath, "remove", "node "+element_to_string(child))
 	
 
 
